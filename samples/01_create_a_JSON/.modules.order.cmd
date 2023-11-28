682dfcc4e08759c43bc2734663e700f8e543dd71
Emanuele Santini <emanuele.santini.88@gmail.com>
2023-11-28 18:27:28 +0100

    Deleting wrong files

--- samples/01_create_a_JSON/.modules.order.cmd
+++
@@ -1,1 +0,0 @@
-savedcmd_/home/emanuele/JsonOnKernel/samples/01_create_a_JSON/modules.order := {   echo /home/emanuele/JsonOnKernel/samples/01_create_a_JSON/test.o; :; } > /home/emanuele/JsonOnKernel/samples/01_create_a_JSON/modules.order
